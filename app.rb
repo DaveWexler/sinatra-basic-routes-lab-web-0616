4d1ea663a791921d1fa972a9998fd97b20c23024
DaveWexler <dwexler@gmail.com>
2016-06-27 11:44:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Dave."
+  end
+
+  get '/hometown' do
+    "My hometown is Westport, CT."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Fat Bottomed Girls, by Queen."
+  end
 end
